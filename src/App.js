64f975d8c1f6a0d102ec8d067015aff68c87a79f
Łukasz <riraseth@gmail.com>
2020-02-22 14:07:59 +0100

    inputs working

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+
+class Form extends Component {
+  state = {
+    firstName: "",
+    lastName: "",
+    people: []
+  };
+
+  //name of input and key in state should be the same
+  handleChange = e => {
+    // if (e.target.name === "firstName") {
+    //   const textValue = e.target.value;
+    //   this.setState({
+    //     firstName: textValue
+    //   });
+    // }
+
+    this.setState({
+      [e.target.name]: [e.target.value]
+    });
+  };
+  render() {
+    return (
+      <section>
+        <article>
+          <form>
+            <input
+              type="text"
+              name="firstName"
+              value={this.state.firstName}
+              onChange={this.handleChange}
+            />
+            <input
+              type="text"
+              name="lastName"
+              value={this.state.lastName}
+              onChange={this.handleChange}
+            />
+            <button type="submit">submit</button>
+          </form>
+        </article>
+        <article>
+          <h1>people</h1>
+          <div>{this.state.people}</div>
+        </article>
+      </section>
+    );
+  }
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <Form></Form>;
 }
 
 export default App;
